c7c569dd2070149a27729b8f575db49716cdad0b
tabjen99 <tabjen99@hotmail.com>
2016-10-10 16:28:27 +0800

    Add files via upload

---
+++ goBiker/test.swift
@@ -0,0 +1,25 @@
+//
+//  test.swift
+//  goBiker
+//
+//  Created by Mike on 10/10/2016.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class testViewController : UIViewController
+{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+    
